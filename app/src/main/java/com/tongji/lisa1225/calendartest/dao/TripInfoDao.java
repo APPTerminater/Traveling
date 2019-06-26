1fa7cbf0a6574dc8d72403b4b6451011be03dc5d
APPTerminater <42427958+APPTerminater@users.noreply.github.com>
2019-06-26 15:32:32 +0800

    TripInfoDao&UserInfoDdao静态测试

--- app/src/main/java/com/tongji/lisa1225/calendartest/dao/TripInfoDao.java
+++ app/src/main/java/com/tongji/lisa1225/calendartest/dao/TripInfoDao.java
@@ -14,26 +14,26 @@ import java.util.Date;
 import java.util.List;
 
 public class TripInfoDao {
-    private MyDBHelper mMyDBHelper;
+    private MyDBHelper myDbHelper;
 
-    /**
+    /*
      * dao类需要实例化数据库Help类,只有得到帮助类的对象我们才可以实例化 SQLiteDatabase
      * @param context
      */
     public TripInfoDao(Context context) {
-        mMyDBHelper=new MyDBHelper(context);
+        myDbHelper = new MyDBHelper(context);
     }
 
     // 将数据库打开帮帮助类实例化，然后利用这个对象
     // 调用谷歌的api去进行增删改查
 
     // 增加的方法，返回的的是一个long值
-    public long addData(TripInfo tripInfo){
+    public long addData(TripInfo tripInfo) {
         // 增删改查每一个方法都要得到数据库，然后操作完成后一定要关闭
         // getWritableDatabase(); 执行后数据库文件才会生成
         // 数据库文件利用DDMS可以查看，在 data/data/包名/databases 目录下即可查看
-        SQLiteDatabase sqLiteDatabase =  mMyDBHelper.getWritableDatabase();
-        ContentValues contentValues=new ContentValues();
+
+        ContentValues contentValues = new ContentValues();
 
         contentValues.put(TripInfo.KEY_nickname,tripInfo.nickname);
         contentValues.put(TripInfo.KEY_destination,tripInfo.destination);
@@ -42,28 +42,32 @@ public class TripInfoDao {
         contentValues.put(TripInfo.KEY_budget,tripInfo.budget);
         contentValues.put(TripInfo.KEY_brief_info,tripInfo.brief_info);
         contentValues.put(TripInfo.KEY_remind,tripInfo.remind);
-        if (tripInfo.total_day>0)
-        contentValues.put(TripInfo.KEY_total_day,tripInfo.total_day);
-        if(!TextUtils.isEmpty(tripInfo.destination))
-        contentValues.put(TripInfo.KEY_memo,tripInfo.memo);
+        if (tripInfo.total_day > 0) {
+            contentValues.put(TripInfo.KEY_total_day,tripInfo.total_day);
+        }
+
+        if (!TextUtils.isEmpty(tripInfo.destination)) {
+            contentValues.put(TripInfo.KEY_memo, tripInfo.memo);
+        }
         // 返回,显示数据添加在第几行
         // 加了现在连续添加了3行数据,突然删掉第三行,然后再添加一条数据返回的是4不是3
         // 因为自增长
-        long rowid=sqLiteDatabase.insert(TripInfo.TABLE,null,contentValues);
+        SQLiteDatabase sqLiteDatabase =  myDbHelper.getWritableDatabase();
+        long rowid = sqLiteDatabase.insert(TripInfo.TABLE,null,contentValues);
 
         sqLiteDatabase.close();
         return rowid;
     }
 
-    /**
+    /*
      * 修改的方法（明明用过了）
      * @param nickname
      * @param starttime
      * @return
     */
-    public int updateRemind(String nickname, Date starttime){
-        SQLiteDatabase sqLiteDatabase = mMyDBHelper.getWritableDatabase();
-        ContentValues contentValues =new ContentValues();
+    public int updateRemind(String nickname, Date starttime) {
+        SQLiteDatabase sqLiteDatabase = myDbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
         contentValues.put(TripInfo.KEY_remind, "no");
         int updateResult = sqLiteDatabase.update(TripInfo.TABLE, contentValues, "nickname=? and start_time=?",
                 new String[]{nickname,String.valueOf(starttime.getTime())});
@@ -71,9 +75,9 @@ public class TripInfoDao {
         return updateResult;
     }
 
-    public int updateRate(TripInfo tripInfo){
-        SQLiteDatabase sqLiteDatabase = mMyDBHelper.getWritableDatabase();
-        ContentValues contentValues =new ContentValues();
+    public int updateRate(TripInfo tripInfo) {
+        SQLiteDatabase sqLiteDatabase = myDbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
         contentValues.put(TripInfo.KEY_comment, tripInfo.comment);
         contentValues.put(TripInfo.KEY_rates, tripInfo.rates);
         contentValues.put(TripInfo.KEY_walk,tripInfo.total_walk);
@@ -87,37 +91,39 @@ public class TripInfoDao {
 
 
 
-    /**
+    /*
      * 查询的方法
      * @param nickname
      * @return
      */
-    public List<TripInfo> alterData(String nickname){
-        List<TripInfo> tripInfoList=new ArrayList<>();
+    public List<TripInfo> alterData(String nickname) {
+        List<TripInfo> tripInfoList = new ArrayList<>();
 
-        int count=0;
-        SQLiteDatabase readableDatabase = mMyDBHelper.getReadableDatabase();
+        int count = 0;
+        SQLiteDatabase readableDatabase = myDbHelper.getReadableDatabase();
         // 查询比较特别,涉及到 cursor
-        Cursor cursor = readableDatabase.query(TripInfo.TABLE, new String[]{TripInfo.KEY_id,TripInfo.KEY_destination,TripInfo.KEY_start_time,
-                TripInfo.KEY_end_time,TripInfo.KEY_budget,TripInfo.KEY_brief_info,TripInfo.KEY_remind,TripInfo.KEY_memo,
-                TripInfo.KEY_cost,TripInfo.KEY_total_day,TripInfo.KEY_walk,TripInfo.KEY_rates,TripInfo.KEY_comment},
+        Cursor cursor = readableDatabase.query(TripInfo.TABLE,
+                new String[]{TripInfo.KEY_id,TripInfo.KEY_destination,TripInfo.KEY_start_time,
+                    TripInfo.KEY_end_time,TripInfo.KEY_budget,TripInfo.KEY_brief_info,TripInfo.KEY_remind,
+                    TripInfo.KEY_memo, TripInfo.KEY_cost,TripInfo.KEY_total_day,TripInfo.KEY_walk,
+                    TripInfo.KEY_rates,TripInfo.KEY_comment},
                 "nickname=?", new String[]{nickname}, null, null, null);
         while (cursor.moveToNext()) {
             TripInfo tripInfo = new TripInfo();
-            tripInfo.nickname=nickname;
+            tripInfo.nickname = nickname;
             tripInfo.id = cursor.getInt(0); //获取第一列的值,第一列的索引从0开始
             tripInfo.destination = cursor.getString(1);//获取第二列的值
             tripInfo.start_time = cursor.getLong(2);
-            tripInfo.end_time=cursor.getLong(3);
-            tripInfo.budget=cursor.getInt(4);
-            tripInfo.brief_info=cursor.getString(5);
-            tripInfo.remind=cursor.getString(6);
-            tripInfo.memo=cursor.getString(7);
-            tripInfo.total_cost=cursor.getInt(8);
-            tripInfo.total_day=cursor.getInt(9);
-            tripInfo.total_walk=cursor.getInt(10);
-            tripInfo.rates=cursor.getFloat(11);
-            tripInfo.comment=cursor.getString(12);
+            tripInfo.end_time = cursor.getLong(3);
+            tripInfo.budget = cursor.getInt(4);
+            tripInfo.brief_info = cursor.getString(5);
+            tripInfo.remind = cursor.getString(6);
+            tripInfo.memo = cursor.getString(7);
+            tripInfo.total_cost = cursor.getInt(8);
+            tripInfo.total_day = cursor.getInt(9);
+            tripInfo.total_walk = cursor.getInt(10);
+            tripInfo.rates = cursor.getFloat(11);
+            tripInfo.comment = cursor.getString(12);
             tripInfoList.add(tripInfo);
             count++;
         }
--- app/src/main/java/com/tongji/lisa1225/calendartest/dao/UserInfoDao.java
+++ app/src/main/java/com/tongji/lisa1225/calendartest/dao/UserInfoDao.java
@@ -9,42 +9,42 @@ import com.tongji.lisa1225.calendartest.dbhelper.MyDBHelper;
 import com.tongji.lisa1225.calendartest.model.UserInfo;
 
 public class UserInfoDao {
-    private MyDBHelper mMyDBHelper;
+    private MyDBHelper myDbHelper;
 
-    /**
+    /*
      * dao类需要实例化数据库Help类,只有得到帮助类的对象我们才可以实例化 SQLiteDatabase
      * @param context
      */
     public UserInfoDao(Context context) {
-        mMyDBHelper=new MyDBHelper(context);
+        myDbHelper = new MyDBHelper(context);
     }
 
     // 将数据库打开帮帮助类实例化，然后利用这个对象
     // 调用谷歌的api去进行增删改查
     
     // 增加的方法，返回的的是一个long值
-    public long addData(String nickname,String birthday,String password,String walk_daliy){
+    public long addData(String nickname,String birthday,String password,String walkDaliy) {
         // 增删改查每一个方法都要得到数据库，然后操作完成后一定要关闭
         // getWritableDatabase(); 执行后数据库文件才会生成
         // 数据库文件利用DDMS可以查看，在 data/data/包名/databases 目录下即可查看
-        SQLiteDatabase sqLiteDatabase =  mMyDBHelper.getWritableDatabase();
-        ContentValues contentValues=new ContentValues();
+        SQLiteDatabase sqLiteDatabase =  myDbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
 
         contentValues.put(UserInfo.KEY_nickname,nickname);
-        contentValues.put(UserInfo.KEY_birthday,birthday );
+        contentValues.put(UserInfo.KEY_birthday,birthday);
         contentValues.put(UserInfo.KEY_password,password);
-        contentValues.put(UserInfo.KEY_walk,Integer.valueOf(walk_daliy));
+        contentValues.put(UserInfo.KEY_walk,Integer.valueOf(walkDaliy));
         contentValues.put(UserInfo.KEY_mode,"day");
         // 返回,显示数据添加在第几行
         // 加了现在连续添加了3行数据,突然删掉第三行,然后再添加一条数据返回的是4不是3
         // 因为自增长
-        long rowid=sqLiteDatabase.insert(UserInfo.TABLE,null,contentValues);
+        long rowid = sqLiteDatabase.insert(UserInfo.TABLE,null,contentValues);
 
         sqLiteDatabase.close();
         return rowid;
     }
 
-    /**
+    /*
      * 修改的方法
      * @param nickname
      * @param newBirthday
@@ -52,108 +52,118 @@ public class UserInfoDao {
      * @param newWalk
      * @return
      */
-    public int updateData(String nickname,String newBirthday,String newPassword,String newWalk){
-        SQLiteDatabase sqLiteDatabase = mMyDBHelper.getWritableDatabase();
-        ContentValues contentValues =new ContentValues();
+    public int updateData(String nickname,String newBirthday,String newPassword,String newWalk) {
+        ContentValues contentValues = new ContentValues();
         contentValues.put(UserInfo.KEY_birthday,newBirthday);
         if (!newPassword.isEmpty()) {
             contentValues.put(UserInfo.KEY_password, newPassword);
         }
         contentValues.put(UserInfo.KEY_walk, newWalk);
+        SQLiteDatabase sqLiteDatabase = myDbHelper.getWritableDatabase();
         int updateResult = sqLiteDatabase.update(UserInfo.TABLE, contentValues, "nickname=?", new String[]{nickname});
         sqLiteDatabase.close();
         return updateResult;
     }
-    /**
+
+    /*
      * 修改的方法（修改模式）
      * @param nickname
      * @param mode
      * @return
      */
-    public int updateMode(String nickname,String mode){
-        SQLiteDatabase sqLiteDatabase = mMyDBHelper.getWritableDatabase();
-        ContentValues contentValues =new ContentValues();
+    public int updateMode(String nickname,String mode) {
+        SQLiteDatabase sqLiteDatabase = myDbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
         contentValues.put(UserInfo.KEY_mode, mode);
         int updateResult = sqLiteDatabase.update(UserInfo.TABLE, contentValues, "nickname=?", new String[]{nickname});
         sqLiteDatabase.close();
         return updateResult;
     }
-    /**
+
+    /*
      * 查询的方法（查找模式）
      * @param nickname
      * @return
      */
-    public String alterMode(String nickname){
+    public String alterMode(String nickname) {
         String mode = null;
 
-        SQLiteDatabase readableDatabase = mMyDBHelper.getReadableDatabase();
+        SQLiteDatabase readableDatabase = myDbHelper.getReadableDatabase();
         // 查询比较特别,涉及到 cursor
 
-        Cursor cursor = readableDatabase.query(UserInfo.TABLE, new String[]{"prefer_mode"}, "nickname=?", new String[]{nickname}, null, null, null);
-        if(cursor.moveToNext()){
-            mode=cursor.getString(0);
+        Cursor cursor = readableDatabase.query(UserInfo.TABLE,
+                new String[]{"prefer_mode"}, "nickname=?",
+                new String[]{nickname}, null, null, null);
+        if (cursor.moveToNext()) {
+            mode = cursor.getString(0);
         }
 
         cursor.close(); // 记得关闭 cursor
         readableDatabase.close(); // 关闭数据库
         return mode;
     }
-    /**
+
+    /*
      * 查询的方法（查找密码）
      * @param nickname
      * @return
      */
-    public String alterPassword(String nickname){
+    public String alterPassword(String nickname) {
         String password = null;
 
-        SQLiteDatabase readableDatabase = mMyDBHelper.getReadableDatabase();
+        SQLiteDatabase readableDatabase = myDbHelper.getReadableDatabase();
         // 查询比较特别,涉及到 cursor
 
-        Cursor cursor = readableDatabase.query(UserInfo.TABLE, new String[]{"password"}, "nickname=?", new String[]{nickname}, null, null, null);
-        if(cursor.moveToNext()){
-            password=cursor.getString(0);
+        Cursor cursor = readableDatabase.query(UserInfo.TABLE, new String[]{"password"},
+                "nickname=?", new String[]{nickname}, null, null, null);
+        if (cursor.moveToNext()) {
+            password = cursor.getString(0);
         }
         cursor.close(); // 记得关闭 cursor
         readableDatabase.close(); // 关闭数据库
         return password;
     }
-    /**
+
+    /*
      * 查询的方法（查找生日）
      * @param nickname
      * @return
      */
-    public String alterBirthday(String nickname){
+    public String alterBirthday(String nickname) {
         String birthday = null;
 
-        SQLiteDatabase readableDatabase = mMyDBHelper.getReadableDatabase();
+        SQLiteDatabase readableDatabase = myDbHelper.getReadableDatabase();
         // 查询比较特别,涉及到 cursor
 
-        Cursor cursor = readableDatabase.query(UserInfo.TABLE, new String[]{"birthday"}, "nickname=?", new String[]{nickname}, null, null, null);
-        if(cursor.moveToNext()){
-            birthday=cursor.getString(0);
+        Cursor cursor = readableDatabase.query(UserInfo.TABLE, new String[]{"birthday"},
+                "nickname=?", new String[]{nickname}, null, null, null);
+        if (cursor.moveToNext()) {
+            birthday = cursor.getString(0);
         }
         cursor.close(); // 记得关闭 cursor
         readableDatabase.close(); // 关闭数据库
         return birthday;
     }
-    /**
+
+    /*
      * 查询的方法（查找步数）
      * @param nickname
      * @return
      */
-    public int alterWalk(String nickname){
-        int walk_daliy = 0;
+    public int alterWalk(String nickname) {
+        int walkDaliy = 0;
 
-        SQLiteDatabase readableDatabase = mMyDBHelper.getReadableDatabase();
+        SQLiteDatabase readableDatabase = myDbHelper.getReadableDatabase();
         // 查询比较特别,涉及到 cursor
 
-        Cursor cursor = readableDatabase.query(UserInfo.TABLE, new String[]{"walk_daliy"}, "nickname=?", new String[]{nickname}, null, null, null);
-        if(cursor.moveToNext()){
-            walk_daliy=cursor.getInt(0);
+        Cursor cursor = readableDatabase.query(UserInfo.TABLE, new String[]{"walk_daliy"},
+                "nickname=?", new String[]{nickname}, null, null, null);
+        if (cursor.moveToNext()) {
+            walkDaliy = cursor.getInt(0);
         }
         cursor.close(); // 记得关闭 cursor
         readableDatabase.close(); // 关闭数据库
-        return walk_daliy;
+        return walkDaliy;
     }
 
 }
